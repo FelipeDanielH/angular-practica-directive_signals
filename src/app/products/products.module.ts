e4af40cc46b1e8076108634230db9c2d267bb12b
Felipe <felipe.daniel.henriquez@gmail.com>
2023-11-02 18:40:30 -0300

    feat(producto): agrega formulario reactivo a la pagina

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -12,7 +13,8 @@ import { ProductPageComponent } from './pages/product-page/product-page.componen
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
