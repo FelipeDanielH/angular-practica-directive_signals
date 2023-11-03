1a2592e98ac35c70bc6c219d31912320cfa7212d
Felipe <felipe.daniel.henriquez@gmail.com>
2023-11-03 15:12:32 -0300

    feat(side-menu): refactor a menu con signals

--- src/app/signals/components/side-menu/side-menu.component.ts
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface MenuItem {
   title: string;
@@ -11,12 +11,17 @@ interface MenuItem {
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
-  public menuItems: MenuItem[] = [
+
+  public menuItems = signal<MenuItem[]>([
+    {title: 'Contador', route: 'counter'},
+    {title: 'Usuario', route: 'user-info'},
+    {title: 'Mutaciones', route: 'properties'}
+  ]);
+
+ /*  public menuItems: MenuItem[] = [
     {title: 'Contador', route: 'counter'},
     {title: 'Usuario', route: 'user-info'},
     {title: 'Mutaciones', route: 'properties'}
   ];
-
-
-
+ */
 }
