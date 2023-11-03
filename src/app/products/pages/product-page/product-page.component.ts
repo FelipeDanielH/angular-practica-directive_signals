02e8d4d80cea10eb2cde2c958aadeb33216873d5
Felipe <felipe.daniel.henriquez@gmail.com>
2023-11-03 10:48:31 -0300

    feat(producto): agrega mensajes de validacion por directiva

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -9,7 +9,7 @@ export class ProductPageComponent {
 
   private fb = inject( FormBuilder );
 
-  public color:string = 'green';
+  public color:string = 'red';
 
   public myForm: FormGroup = this.fb.group({
     name: [ '', [ Validators.required, Validators.minLength(6), Validators.email ] ]
--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,18 +1,30 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
 })
 export class CustomLabelDirective implements OnInit {
 
+  // variables a utiliza
   private htmlElement?: ElementRef<HTMLElement>
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null
 
+  // metodos input
   @Input() set color(value: string) {
     this._color = value;
+
     this.setStyle();
   }
 
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value
+    this.clearMessages();
+    this.setErrorMessage();
+  }
+
+  // ciclo de vida
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el
   }
@@ -21,8 +33,41 @@ export class CustomLabelDirective implements OnInit {
     this.setStyle()
   }
 
+  // metodos personalizados
   setStyle(): void {
-    if( !this.htmlElement) return;
+    if (!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerText = ''
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    errors.forEach(error => {
+      switch (error) {
+        case 'required':
+          this.htmlElement!.nativeElement.innerText = this.htmlElement!.nativeElement.innerText.concat('Este elemento es requerido \n')
+          break;
+        case 'minlength':
+          this.htmlElement!.nativeElement.innerText = this.htmlElement!.nativeElement.innerText.concat(`Minimo 6 caracteres (Actualmente: ${this._errors!['minlength'].actualLength}) \n`)
+          break;
+        case 'email':
+          this.htmlElement!.nativeElement.innerText = this.htmlElement!.nativeElement.innerText.concat('Debe ser un email \n')
+          break;
+      }
+    });
+  }
+
+  clearMessages(): void {
+    if (!this.htmlElement) return;
+    if (this._errors) {
+      this.htmlElement.nativeElement.innerText = ''
+      return;
+    }
   }
 }
