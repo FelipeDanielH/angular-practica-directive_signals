a8e295db0f5f96b54c9562b99a4c959629f1d7d3
Felipe <felipe.daniel.henriquez@gmail.com>
2023-11-03 20:18:11 -0300

    feat(user-info): agrega manejo de errores

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,32 +1,43 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { UsersService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interface';
+import { first } from 'rxjs';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrls: ['./user-info-page.component.css']
 })
-export class UserInfoPageComponent implements OnInit{
+export class UserInfoPageComponent implements OnInit {
   private userService = inject(UsersService);
   public userId = signal(1);
 
-  public currentUser = signal<User|undefined>(undefined);
+  public currentUser = signal<User | undefined>(undefined);
   public userWasFound = signal(true);
+  public fullName = computed<string>(() => {
+    if (this.currentUser()) return 'Usuario no encontrado';
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`
+  })
 
   ngOnInit(): void {
-    this.loadUser( this.userId() );
+    this.loadUser(this.userId());
   }
 
-  loadUser(id: number){
-    if( id <= 0 ) return;
+  loadUser(id: number) {
+    if (id <= 0) return;
 
     this.userId.set(id);
-
     this.currentUser.set(undefined);
 
-    this.userService.getUserById( id )
-      .subscribe( user => {
-        this.currentUser.set(user);
+    this.userService.getUserById(id)
+      .subscribe({
+        next: (user) => {
+          this.currentUser.set(user);
+          this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false)
+          this.currentUser.set(undefined)
+        }
       })
   }
 
