72508669fc8564da65b24e3ac103933106ca0fa8
Felipe <felipe.daniel.henriquez@gmail.com>
2023-11-03 12:29:42 -0300

    feat(side-menu): agrega side-menu

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: 'side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css']
+})
+export class SideMenuComponent {
+  public menuItems: MenuItem[] = [
+    {title: 'Contador', route: 'counter'},
+    {title: 'Usuario', route: 'user-info'},
+    {title: 'Mutaciones', route: 'properties'}
+  ];
+
+
+
+}
